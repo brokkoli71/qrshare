c241095dad2b79b171449e152bb2a103247b0ec1
Hannes Spitz <44113112+brokkoli71@users.noreply.github.com>
2024-09-09 01:49:17 +0200

    add encryption

--- index.php
+++ index.php
@@ -4,13 +4,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js"></script>
+
     <?php
         include 'config.php';
         require 'db.php';
 
+        if(isset($_POST['pwd'])) $pwd=$_POST['pwd'];
+        if(isset($_GET['pwd'])) $pwd=$_GET['pwd'];
+        if ($pwd) $locked="🔒";
+
         if(isset($_POST['session'])) $session=$_POST['session'];
         if(isset($_GET['session'])) $session=$_GET['session'];
         if ($session=="") $session=random_int(0, pow(2,16));
+
+        $cipher = "aes-128-gcm";
     ?>
     <script>
         document.addEventListener("DOMContentLoaded", function(){
@@ -23,17 +31,19 @@
 </head>
 <body>
     <div class="header">
-        <h1>Quick Share Text</h1>
+        <h1>Quick Share Text<?php echo " $locked";?></h1>
         <div class="row">
             <p>Paste some text and press "Send" to share it with your devices. Press "Receive" to reload all shared texts in this room</p>
             <form action="index.php" method="post">
                 <input type="text" id="shared" name="shared" required>
-                <input type="submit" id="shared" value="Send" class=".button-13">
+                <input type="submit" id="shared" value="Send<?php echo " $locked";?>" class=".button-13">
                 <input type="hidden" id="session" name="session" value='<?php echo "$session";?>'>
+                <input type="hidden" id="pwd" name="pwd" value='<?php echo "$pwd";?>'>
             </form>
             <form action="index.php" method="post">
                 <input type="submit" id="shared" value="Receive" class=".button-13">
                 <input type="hidden" id="session" name="session" value='<?php echo "$session";?>'>
+                <input type="hidden" id="pwd" name="pwd" value='<?php echo "$pwd";?>'>
             </form>
         </div>
     </div>
@@ -52,15 +62,28 @@
                     // Collect form data
                     $content = $_POST['shared'];
     
-                    //print_r($_POST);
-                    // Prepare the SQL query
-                    $stmt2 = $db->prepare("INSERT INTO QRshared(content, session) VALUES (?,?)");
-    
-                    // Bind the parameter
-                    $stmt2->bind_param('si', $content, $session);
-                    // Execute the query
-                    if (!$stmt2->execute()) {
-                        echo "Error sending<br>";
+                    //encrypt
+                    if (in_array($cipher, openssl_get_cipher_methods())){
+                        $ivlen = openssl_cipher_iv_length($cipher);
+                        $iv = openssl_random_pseudo_bytes($ivlen);
+                        $ciphertext = openssl_encrypt($content, $cipher, $pwd, $options=0, $iv, $tag);
+                        // speichere $cipher, $iv und $tag für spätere Entschlüsselung
+                        $original_plaintext = openssl_decrypt($ciphertext, $cipher, $pwd, $options=0, $iv, $tag);
+                        echo $original_plaintext."\n";
+                        echo $ciphertext;
+                        echo $ciphertext, $cipher, $key, $options=0, $iv, $tag;
+
+                        // Prepare the SQL query
+                        $stmt2 = $db->prepare("INSERT INTO QRshared(content, session, enc_iv, enc_tag) VALUES (?,?,?,?)");
+                        
+                        // Bind the parameter
+                        $stmt2->bind_param('siss', $ciphertext, $session, base64_encode($iv), base64_encode($tag));
+                        // Execute the query
+                        if (!$stmt2->execute()) {
+                            echo "Error sending<br>";
+                        }
+                    }else{
+                        echo $cipher + "is no valid encryption algo";
                     }
                 }
             ?>
@@ -68,18 +91,20 @@
                 
                 <?php
                     //collect all contents from session
-                    $stmt = $db->prepare("SELECT content FROM QRshared WHERE session = ?");
+                    $stmt = $db->prepare("SELECT content, enc_iv, enc_tag FROM QRshared WHERE session = ?");
                     $stmt->bind_param('i', $session);
                     if (!$stmt->execute()) {
                         echo "error reading shared data<br>";
                     }
                     $stmt->store_result();
     
-                    $stmt->bind_result($read_content);
+                    $stmt->bind_result($read_content, $iv_b64, $tag_b64);
                     echo "<ul>";
                     if($stmt->num_rows > 0) {
                         while ($stmt->fetch()) {
-                            printf("<li>%s</li>", $read_content); 
+                            $original_plaintext = openssl_decrypt($read_content, $cipher, $pwd, $options=0, base64_decode($iv_b64), base64_decode($tag_b64));
+                            if ($original_plaintext) printf("<li>%s</li>", $original_plaintext); 
+                            else printf("<li>could not decrypt using current password %s</li>", $read_content); 
                         }
                     }
                     echo "</ul>";
@@ -94,8 +119,16 @@
                     <form action="index.php" method="post">
                         <input class="input" style="width:70%" type='number' id='session'  name="session" tabindex='-1' placeholder='session' size=10 value='<?php echo "$session";?>'>
                         <input type='submit' style="width:25%" id="shared" value="change" tabindex='-1' value='ok'>
+                        <input type="hidden" id="pwd" name="pwd" value='<?php echo "$pwd";?>'>
                     </form>
                 </div>
+            </div>
+            <div style="overflow: hidden; width: min(400px, 100%)">
+                <form action="index.php" method="post">
+                    <input class="password" style="width:70%" type='text' id='pwd'  name="pwd" tabindex='-1' placeholder='optional encryption key' size=10 value='<?php echo "$pwd";?>'>
+                    <input type='submit' style="width:25%" id="shared" value="change" tabindex='-1' value='ok'>
+                    <input type="hidden" id="session" name="session" value='<?php echo "$session";?>'>
+                </form>
             </div>
         </div>
         <div class="col-3 right">
@@ -108,7 +141,7 @@
 
         
     <div class="footer">
-        <p>warning: the data is stored in the database temporarily unencrypted.</p>
+        <p>The data will be encrypted using your password and stored in the database. Choose a strong password to secure your data.</p>
     </div>
     <script>
         //change sessionID button
